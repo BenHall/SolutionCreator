2825ba04d6a238146b2743a4fe275a9af5bd23d7
BenHall <ben@benhall.me.uk>
2010-07-25 22:34:13 +0100

    Using SharpZipLib - still the same issue

--- src/SolutionCreator.Commands/Zipper.cs
+++ src/SolutionCreator.Commands/Zipper.cs
@@ -1,7 +1,4 @@
-﻿using System.Diagnostics;
-using System.Text;
-using System.IO;
-using TinySharpZip;
+﻿using ICSharpCode.SharpZipLib.Zip;
 
 namespace SolutionCreator.Commands
 {
@@ -9,18 +6,14 @@ namespace SolutionCreator.Commands
     {
         public void Extract(string file, string extractTo)
         {
-            string argument = @"x " + file + " -o"+extractTo;
-            Process.Start(@"C:\Program Files\7-Zip\7z.exe", argument);
-            //Stream zipStream = new FileStream(file, FileMode.Open);
-            //ZipArchive.Extract(zipStream, extractTo);
-            //zipStream.Close();
+            FastZip zip = new FastZip();
+            zip.ExtractZip(file, extractTo, FastZip.Overwrite.Always, name => true, "", "", false);
         }
 
         public void Zip(string directory, string fileName)
         {
-            Stream outputZipStream = new FileStream(fileName, FileMode.Create);
-            ZipArchive.CreateFromDirectory(directory, outputZipStream, false);
-            outputZipStream.Close();
+            FastZip zip = new FastZip();
+            zip.CreateZip(fileName, directory, true, "");
         }
     }
 }
